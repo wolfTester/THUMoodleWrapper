33442b48df95f293f57f2ad200f451ea98ab00b0
wolfwarrior666 <ian-wilken@mailbox.org>
2023-01-17 09:06:05 +0100

    Created First Version

---
+++ app/src/main/java/de/wolfwarrior/thu_moodle_wrapper/MainActivity.kt
@@ -0,0 +1,26 @@
+package de.wolfwarrior.thu_moodle_wrapper
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val source = intent
+        val i = Intent(Intent.ACTION_VIEW, Uri.parse("moodlemobile://link=" + source.dataString))
+        if (i.resolveActivity(packageManager) != null) {
+            startActivity(i)
+            finish()
+            return
+        } else {
+            Toast.makeText(this, "Newest version of Moodle not installed.", Toast.LENGTH_SHORT)
+                .show()
+        }
+
+
+    }
+}
