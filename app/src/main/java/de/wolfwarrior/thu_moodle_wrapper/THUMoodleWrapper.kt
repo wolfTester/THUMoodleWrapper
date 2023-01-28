5d612853b511aad603f4c6bb655f237bdd1a91e9
wolfwarrior666 <ian-wilken@mailbox.org>
2023-01-28 09:25:40 +0100

    Add Information Activity

---
+++ app/src/main/java/de/wolfwarrior/thu_moodle_wrapper/THUMoodleWrapper.kt
@@ -0,0 +1,11 @@
+package de.wolfwarrior.thu_moodle_wrapper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class THUMoodleWrapper : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thumoodle_wrapper)
+    }
+}
