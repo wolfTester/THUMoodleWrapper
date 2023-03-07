0845771dd4c8589e3c8f35a9f3e8ff35b8c576cd
wolfwarrior <wolfwarrior@wolfwarrior.de>
2023-03-07 23:32:21 +0100

    Change Bugfixe by setup Querry

--- app/src/main/java/de/wolfwarrior/thu_moodle_wrapper/MainActivity.kt
+++ app/src/main/java/de/wolfwarrior/thu_moodle_wrapper/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -16,16 +17,13 @@ class MainActivity : AppCompatActivity() {
         val i = Intent(Intent.ACTION_VIEW, Uri.parse("moodlemobile://link=" + source.dataString))
         val tmp = source.dataString
         if (tmp.toString() != "null") {
-            startActivity(i)
-            finish()
-            return
-            /*if (i.resolveActivity(packageManager) != null) { //Old Version with Bug since Android 11 +
+            if (i.resolveActivity(packageManager) != null) { //Old Version with Bug since Android 11 +
                 startActivity(i)
                 finish()
                 return
             } else {
                 Toast.makeText(this, getString(R.string.no_moodle_app), Toast.LENGTH_SHORT).show()
-            }*/
+            }
         }else{
             setContentView(R.layout.activity_main)
             val webview = findViewById<WebView>(R.id.webview)
